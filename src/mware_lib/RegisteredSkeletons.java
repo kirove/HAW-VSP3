1bdfea6f2a8b48c754cc4b9505e07b9a60f46fb6
Stephan Berngruber <stephan.berngruber@gmx.de>
2013-12-11 14:57:10 +0100

    blafu commit mittendrin macht keinen sinn

---
+++ src/mware_lib/RegisteredSkeletons.java
@@ -0,0 +1,11 @@
+package mware_lib;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: me
+ * Date: 12/11/13
+ * Time: 2:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RegisteredSkeletons {
+}
